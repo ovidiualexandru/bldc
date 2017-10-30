6810474e1ba81d317d861d2bc9656c1b841773b1
Ovidiu Alexandru-B53575 <ovidiu.alexandru@freescale.com>
2017-10-30 12:05:08 +0200

    Initial custom app code for spi-slave interface

---
+++ applications/app_spislave.c
@@ -0,0 +1,43 @@
+/*
+	Copyright 2016 Benjamin Vedder	benjamin@vedder.se
+
+	This file is part of the VESC firmware.
+
+	The VESC firmware is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    The VESC firmware is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    */
+
+#include "app.h"
+#include "ch.h"
+#include "hal.h"
+#include "hw.h"
+#include "packet.h"
+#include "commands.h"
+
+#include <string.h>
+
+
+void app_custom_start(void)
+{
+    /* start the SPI as slave */
+}
+
+void app_custom_stop(void)
+{
+    /* stop the presses */
+}
+
+void app_custom_configure(app_configuration *conf)
+{
+    /* reconfigure - should not be necessary */
+}
